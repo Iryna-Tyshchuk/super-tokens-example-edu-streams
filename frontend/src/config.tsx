2348b3b62b08ec3651337d7d4409e27afc482391
Iryna-Tyshchuk <ukriris93@gmail.com>
2023-05-16 16:34:39 +0300

    Initial commit

---
+++ frontend/src/config.tsx
@@ -0,0 +1,39 @@
+import ThirdParty, { Google, Github, Apple } from "supertokens-auth-react/recipe/thirdparty";
+import { ThirdPartyPreBuiltUI } from "supertokens-auth-react/recipe/thirdparty/prebuiltui";
+import Session from "supertokens-auth-react/recipe/session";
+
+export function getApiDomain() {
+    const apiPort = process.env.REACT_APP_API_PORT || 3001;
+    const apiUrl = process.env.REACT_APP_API_URL || `http://localhost:${apiPort}`;
+    return apiUrl;
+}
+
+export function getWebsiteDomain() {
+    const websitePort = process.env.REACT_APP_WEBSITE_PORT || 3000;
+    const websiteUrl = process.env.REACT_APP_WEBSITE_URL || `http://localhost:${websitePort}`;
+    return websiteUrl;
+}
+
+export const SuperTokensConfig = {
+    appInfo: {
+        appName: "SuperTokens Demo App",
+        apiDomain: getApiDomain(),
+        websiteDomain: getWebsiteDomain(),
+    },
+    // recipeList contains all the modules that you want to
+    // use from SuperTokens. See the full list here: https://supertokens.com/docs/guides
+    recipeList: [
+        ThirdParty.init({
+            signInAndUpFeature: {
+                providers: [Github.init(), Google.init(), Apple.init()],
+            },
+        }),
+        Session.init(),
+    ],
+};
+
+export const recipeDetails = {
+    docsLink: "https://supertokens.com/docs/thirdparty/introduction",
+};
+
+export const PreBuiltUIList = [ThirdPartyPreBuiltUI];
